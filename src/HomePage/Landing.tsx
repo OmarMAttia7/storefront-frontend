7c248c9541b8d763fdd0042edfd4851746eae9ae
hazel97 <omarmattia97@gmail.com>
2022-11-28 22:40:42 +0200

    feat: added landing page

--- src/HomePage/Landing.tsx
+++ src/HomePage/Landing.tsx
@@ -1,8 +1,27 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { LoginStateContext } from "../App";
+import logout from "../utils/logout";
+import "./landing.css"
 function Landing(): JSX.Element {
-  return (
-    <div>
-      <p>Landing</p>
+  const LoginState = useContext(LoginStateContext)[0];
+  const setLoginState = useContext(LoginStateContext)[1];
+  function handleLogout() {
+    logout();
+    setLoginState({
+      isLoggedIn: false,
+      status: "loading"
+    });
+  }
+  return LoginState.isLoggedIn ? (
+    <div className="landing-container">
+      <h1 className="landing-header">
+        Hello, {LoginState.user.firstName} {LoginState.user.lastName}
+      </h1>
+      <button className="landing-logout" onClick={handleLogout}>Logout</button>
     </div>
+  ) : (
+    <Navigate to="/login" />
   );
 }
 
