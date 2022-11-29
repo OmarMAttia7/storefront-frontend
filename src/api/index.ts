35b669239fd20900cf37ff1e2993b6c59fec46e9
hazel97 <omarmattia97@gmail.com>
2022-11-29 19:29:58 +0200

    fix: fix api address

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,2 @@
-const api = "http://storefrontbackend-env.eba-83btxpmu.us-east-2.elasticbeanstalk.com/";
+const api = "http://storefrontbackend-env.eba-83btxpmu.us-east-2.elasticbeanstalk.com";
 export default api;
